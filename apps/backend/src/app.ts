a0069ba2b2ecbc96c14bc9ef957e2f497bddeea8
CristobalSg <cristobal.sandoval2022@alu.uct.cl>
2025-06-15 22:43:02 -0400

    fix 2: deploid, install cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,8 +1,16 @@
-import express from 'express'
+import cors from "cors";
 import helmet from 'helmet'
+import express from 'express'
 import apiRouter from './routes'
 
 const app = express()
+
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://invexa-five.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 app.use(helmet())
 app.use(express.json())
