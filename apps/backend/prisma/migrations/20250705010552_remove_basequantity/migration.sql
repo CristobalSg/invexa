93c8c2aea2b13d121f9580112e0ca76ea1e5cffb
Coto2412 <jdelpino2020@alu.uct.cl>
2025-07-05 23:00:02 -0400

    cambio en migraciones

--- apps/backend/prisma/migrations/20250705010552_remove_basequantity/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Presentation" ALTER COLUMN "description" DROP NOT NULL,
-ALTER COLUMN "unitLabel" DROP NOT NULL;
--- apps/backend/prisma/migrations/20250705011340_remove_basequantity_2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `baseQuantity` on the `Presentation` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Presentation" DROP COLUMN "baseQuantity";
